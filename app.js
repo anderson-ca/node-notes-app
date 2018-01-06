33a2aee5b62da27107bfcec6dd762f356d648516
andersoncardoso <anderson.d.cardoso@hotmail.com>
2018-01-06 12:05:11 -0600

    os and fs modules added.

--- app.js
+++ app.js
@@ -1,2 +1,18 @@
 'use strict';
 console.log('starting app.js...');
+
+const fs = require('fs');
+
+const os = require('os');
+
+let user = os.userInfo;
+
+
+fs.appendFile('greeting.txt', `Hello ${user.username}!`, function (err) {
+        if (err) {
+            console.log('Unable to write to file.');
+        }
+    }
+);
+
+console.log(user.username);
