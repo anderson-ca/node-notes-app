45d8760af824ce474a33bc6c433d243e6aead3ed
andersoncardoso <anderson.d.cardoso@hotmail.com>
2018-01-06 12:21:28 -0600

    add exportation of file inside root folder.

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 'use strict';
 console.log('starting app.js...');
 
+// fs (file system) built-in module. used for I/O file operations. It has both synchronous and asynchronous forms.
 const fs = require('fs');
 
+// os (operating system) build-in module used for operating system related tasks.
 const os = require('os');
+
+// exporting functionality from a file created by myself by using require and passing a absolute path as a argument. Giving me access to methods inside notes.js
+const notes = require('./notes.js');
 
 let user = os.userInfo();
 
